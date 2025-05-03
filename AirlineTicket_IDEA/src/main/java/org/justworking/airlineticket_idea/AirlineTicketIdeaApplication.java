dff7dd5e02e9af94ea5b623197bc725f37e4b8a0
JustWorking <3546124151@qq.com>
2025-05-03 18:22:33 +0800

    创建了目录结构

---
+++ AirlineTicket_IDEA/src/main/java/org/justworking/airlineticket_idea/AirlineTicketIdeaApplication.java
@@ -0,0 +1,13 @@
+package org.justworking.airlineticket_idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineTicketIdeaApplication.class, args);
+    }
+
+}
---
+++ AirlineTicket_IDEA/src/test/java/org/justworking/airlineticket_idea/AirlineTicketIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.justworking.airlineticket_idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineTicketIdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
